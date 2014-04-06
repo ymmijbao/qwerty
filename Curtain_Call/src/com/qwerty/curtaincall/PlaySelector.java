f7a52bf8b9602ed63439ed419b9fe39d8de35f3f
Mushmellow <brentdano@gmail.com>
2014-04-05 17:20:03 -0700

    play selector logic implemented
    ability to add new plays in scroll view

--- Curtain_Call/src/com/qwerty/curtaincall/PlaySelector.java
+++ Curtain_Call/src/com/qwerty/curtaincall/PlaySelector.java
@@ -2,51 +2,109 @@ package com.qwerty.curtaincall;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.support.v4.app.NavUtils;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.view.GestureDetector;
+import android.view.GestureDetector.SimpleOnGestureListener;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class PlaySelector extends Activity {
-
+public class PlaySelector extends Activity implements OnClickListener {
+	private RelativeLayout mainLayout;
+	private LinearLayout scrollLinLayout;
+	private GestureDetector gestureDetector;
+	View.OnTouchListener gestureListener;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.play_selector);
-		// Show the Up button in the action bar.
-		setupActionBar();
+		
+		mainLayout = (RelativeLayout) findViewById(R.id.playSelectorLayout);
+		scrollLinLayout = (LinearLayout) findViewById(R.id.scrollViewLinLayout);
+		
+		gestureDetector = new GestureDetector(getApplicationContext(), new MyGestureDetector());
+        gestureListener = new View.OnTouchListener() {
+            public boolean onTouch(View v, MotionEvent event) {
+                return gestureDetector.onTouchEvent(event);
+            }
+        };
+        
+        Button addNewPlayButton = (Button) findViewById(R.id.add_new_play);
+        addNewPlayButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				final Button newPlay = new Button(PlaySelector.this);
+				newPlay.setBackgroundColor(Color.RED);
+				
+				
+			    AlertDialog.Builder alert = new AlertDialog.Builder(PlaySelector.this);
+			    final EditText input = new EditText(PlaySelector.this);
+			    alert.setView(input);
+			    alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+			        public void onClick(DialogInterface dialog, int whichButton) {
+			            String value = input.getText().toString().trim();
+			            newPlay.setText(value);
+			            scrollLinLayout.addView(newPlay);
+			        }
+			    });
+
+			    alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+			        public void onClick(DialogInterface dialog, int whichButton) {
+			            dialog.cancel();
+			        }
+			    });
+			    
+			    alert.show(); 
+			}
+		});
 	}
+	
+	public class MyGestureDetector extends SimpleOnGestureListener {
+		private static final int SWIPE_MIN_DIST = 120;
+		private static final int SWIPE_MAX_OFF_PATH = 250;
+		private static final int SWIPE_THRESHOLD_VELOCITY = 200;
+		private static final int SWIPE_THRESHOLD = 100;
+		
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+            try {
+                if (Math.abs(e1.getY() - e2.getY()) > SWIPE_MAX_OFF_PATH) return false;
 
-	/**
-	 * Set up the {@link android.app.ActionBar}.
-	 */
-	private void setupActionBar() {
+                if (e1.getX() - e2.getX() > SWIPE_MIN_DIST && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+                	Toast.makeText(PlaySelector.this, "Left SWIPE", Toast.LENGTH_SHORT).show();
+                } else if (e2.getX() - e1.getX() > SWIPE_MIN_DIST && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+                	Toast.makeText(PlaySelector.this, "Right SWIPE", Toast.LENGTH_SHORT).show();
+                	RelativeLayout layout = (RelativeLayout) findViewById(R.id.playSelectorLayout);
+                    TextView test = (TextView) findViewById(R.id.textView1);
+                    layout.removeView(test);
+                }
+                
+            } catch (Exception e) {
+                // Do nothing
+            }
+            return false;
+        }
 
-		getActionBar().setDisplayHomeAsUpEnabled(true);
-
-	}
+        @Override
+        public boolean onDown(MotionEvent e) {
+              return true;
+        }
+    }
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.play_selector, menu);
-		return true;
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
 	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-		case android.R.id.home:
-			// This ID represents the Home or Up button. In the case of this
-			// activity, the Up button is shown. Use NavUtils to allow users
-			// to navigate up one level in the application structure. For
-			// more details, see the Navigation pattern on Android Design:
-			//
-			// http://developer.android.com/design/patterns/navigation.html#up-vs-back
-			//
-			NavUtils.navigateUpFromSameTask(this);
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
-
 }
