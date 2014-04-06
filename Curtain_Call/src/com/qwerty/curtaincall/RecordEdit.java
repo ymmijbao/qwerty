9ac4fdfd8b12c6ea5c8446a38e946399502439f3
Mushmellow <brentdano@gmail.com>
2014-04-05 17:03:04 -0700

    implemented quasi-mode stub

--- Curtain_Call/src/com/qwerty/curtaincall/RecordEdit.java
+++ Curtain_Call/src/com/qwerty/curtaincall/RecordEdit.java
@@ -2,11 +2,19 @@ package com.qwerty.curtaincall;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+import android.widget.Button;
 import android.support.v4.app.NavUtils;
 
 public class RecordEdit extends Activity {
+	
+	Button me;
+	Button them;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +22,15 @@ public class RecordEdit extends Activity {
 		setContentView(R.layout.record_edit);
 		// Show the Up button in the action bar.
 		setupActionBar();
+		
+		// grab the intent
+		Intent intent = getIntent();
+		String value = intent.getStringExtra("key"); //if it's a string you stored.
+		System.out.println(value);
+		
+		// Set up the listeners
+		addListenerOnMeButton();
+		addListenerOnThemButton();
 	}
 
 	/**
@@ -47,6 +64,42 @@ public class RecordEdit extends Activity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+	
+	public void addListenerOnMeButton() {
+		
+		me = (Button) findViewById(R.id.meButton);
+
+		me.setOnTouchListener(new OnTouchListener() {
+			   @Override
+			   public boolean onTouch(View v, MotionEvent event) {
+			       if(event.getAction() == MotionEvent.ACTION_DOWN) {
+			    	   System.out.println("me button pressed down");
+			       } else if (event.getAction() == MotionEvent.ACTION_UP) {
+			    	   System.out.println("me button release");
+			       }
+			       return true;
+			   }
+		});
+
+	}
+	
+	public void addListenerOnThemButton() {
+		
+		them = (Button) findViewById(R.id.themButton);
+
+		them.setOnTouchListener(new OnTouchListener() {
+			   @Override
+			   public boolean onTouch(View v, MotionEvent event) {
+			       if(event.getAction() == MotionEvent.ACTION_DOWN) {
+			    	   System.out.println("them button pressed down");
+			       } else if (event.getAction() == MotionEvent.ACTION_UP) {
+			    	   System.out.println("them button release");
+			       }
+			       return true;
+			   }
+		});
+
 	}
 
 }
--- Curtain_Call/src/com/qwerty/curtaincall/SplashScreen.java
+++ Curtain_Call/src/com/qwerty/curtaincall/SplashScreen.java
@@ -1,15 +1,24 @@
 package com.qwerty.curtaincall;
 
+
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class SplashScreen extends Activity {
+	
+	Button stub;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.splash_screen);
+		addListenerOnButton();
 	}
 
 	@Override
@@ -17,6 +26,24 @@ public class SplashScreen extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	public void addListenerOnButton() {
+		
+		stub = (Button) findViewById(R.id.button1);
+
+		stub.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+				System.out.println("button clicked");
+				Intent myIntent = new Intent(SplashScreen.this, RecordEdit.class);
+				myIntent.putExtra("key", "WHODUNIT"); //Optional parameters
+				SplashScreen.this.startActivity(myIntent);
+			}
+
+		});
+
 	}
 
 }
