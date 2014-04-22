2ef4ca35619fb4e1abbf5f15f7691732dedbf32e
Haley Rowland <haleyalexisr@gmail.com>
2014-04-22 16:16:22 -0700

    Rehearse Button deleted?

--- Curtain_Call/src/com/qwerty/curtaincall/RecordEdit.java
+++ Curtain_Call/src/com/qwerty/curtaincall/RecordEdit.java
@@ -78,9 +78,6 @@ public class RecordEdit extends Activity {
 		addListenerOnSaveButton();
 		addListenerOnCancelButton();
 		
-		// TODO Sol added:
-		addListenerOnRehearseButton();
-		
 		// Set up audio recorder
 		
 		/*
@@ -370,19 +367,4 @@ public class RecordEdit extends Activity {
 		});
 
 	}
-	
-	// TODO Sol added:
-	public void addListenerOnRehearseButton() {
-		final Button rehearseButton = (Button)findViewById(R.id.rehearseButton);
-		rehearseButton.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				Intent intent = new Intent(RecordEdit.this, RehearseActivity.class);
-				intent.putExtra("play", playName);
-				intent.putExtra("chunk", chunkName);
-				startActivity(intent);
-			}
-		});
-	}
-
 }
